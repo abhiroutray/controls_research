4e735bd165e4ab61fa6d29d81a67b8d59b563945
Abhishek Routray <abhishek_routray@hotmail.com>
2019-03-08 15:35:02 -0600

    First commit

---
+++ Giannis/catkin_ws/build/vicon_bridge/CMakeFiles/vicon_bridge_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vicon_bridge_generate_messages_cpp"
+  "/home/ubuntu/catkin_ws/devel/include/vicon_bridge/Markers.h"
+  "/home/ubuntu/catkin_ws/devel/include/vicon_bridge/Marker.h"
+  "/home/ubuntu/catkin_ws/devel/include/vicon_bridge/TfDistortInfo.h"
+  "/home/ubuntu/catkin_ws/devel/include/vicon_bridge/viconGrabPose.h"
+  "/home/ubuntu/catkin_ws/devel/include/vicon_bridge/viconCalibrateSegment.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/vicon_bridge_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Giannis/catkin_ws/build/vicon_bridge/CMakeFiles/vicon_bridge_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vicon_bridge_generate_messages_nodejs"
+  "/home/ubuntu/catkin_ws/devel/share/gennodejs/ros/vicon_bridge/msg/Markers.js"
+  "/home/ubuntu/catkin_ws/devel/share/gennodejs/ros/vicon_bridge/msg/Marker.js"
+  "/home/ubuntu/catkin_ws/devel/share/gennodejs/ros/vicon_bridge/msg/TfDistortInfo.js"
+  "/home/ubuntu/catkin_ws/devel/share/gennodejs/ros/vicon_bridge/srv/viconGrabPose.js"
+  "/home/ubuntu/catkin_ws/devel/share/gennodejs/ros/vicon_bridge/srv/viconCalibrateSegment.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/vicon_bridge_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
